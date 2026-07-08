5024318808124cad5570d6c745ba39042b29b989
User-MiMa <mlsmasson@outlook.com>
2026-07-08 17:17:24 -0600

    feat: add Joi package to validate creation data

---
+++ src/controllers/inputValidator.js
@@ -0,0 +1,16 @@
+import Joi from "joi";
+
+const userScheme = Joi.object({
+    name: Joi.string().min(3).required(),
+    email: Joi.string().email().required,
+}); 
+
+export const validateUser = function(req, res, next){
+    const { error } = userScheme.validate(req.body);
+    if(error){return res.status(400).json({
+        status: 400,
+        message: error.details[0].message,
+        });
+    }
+    next();
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from '../controllers/userController.js';
+import { validateUser } from '../controllers/inputValidator.js';
 
 export const router = express.Router();
 
 router.get("/user",getAllUsers);
-router.post("/user",createUser);
+router.post("/user",validateUser,createUser);
 
 router.get("/user/:id",getUserById);
-router.put("/user/:id",updateUser);
+router.put("/user/:id",validateUser,updateUser);
 
 router.delete("/user/:id",deleteUser);
 
