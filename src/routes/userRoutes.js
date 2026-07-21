564a0823d77a34a51c7555192dc339951b49b51e
User-MiMa <mlsmasson@outlook.com>
2026-07-20 19:14:24 -0600

    feat: protect routes with admin login/token

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import { loginAdmin, createAdmin, createUser, deleteUser, getAllUsers, getUserById, updateUser } from '../controllers/userController.js';
 import { validateUser } from '../controllers/inputValidator.js';
+import { authenticateToken } from '../middleware/auth.js';
 
 export const router = express.Router();
 
 router.get("/user",getAllUsers);
-router.post("/user",validateUser,createUser);
+router.post("/user",validateUser,authenticateToken,createUser);
 
 router.get("/user/:id",getUserById);
-router.put("/user/:id",validateUser,updateUser);
+router.put("/user/:id",validateUser,authenticateToken,updateUser);
 
-router.delete("/user/:id",deleteUser);
+router.delete("/user/:id",authenticateToken,deleteUser);
 
 router.post("/admin",createAdmin);
 
